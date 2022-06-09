f3c131f653abd6363fea1225950bd50c2e7308c1
Nishant8826 <rnishant609@gmail.com>
2022-06-09 23:54:18 +0530

    DONE

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,36 @@
 const authorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
+const publisherModel=require("../models/publisherModel")
 
+//Q.3
 const createBook= async function (req, res) {
-    let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
+    let bookData = req.body;
+    let autherIDFinder =req.body.author;
+    // console.log(autherIDFinder)
+    let publisherIDFinder =req.body.publisher;
+    
+    let findAuther=await authorModel.findOne({_id:autherIDFinder});
+    // console.log(findAuther)
+    let findPublish=await publisherModel.findOne({_id:publisherIDFinder});
+    
+    if(findAuther===null || findPublish===null){
+        res.send("Invalid Input");
+    }
+    else if(findAuther===undefined || findPublish===undefined){
+        res.send("Detail Is Required");
+    }else{
+        let bookCreated = await bookModel.create(bookData);
+        res.send({data: bookCreated});
+    }
+    
 }
 
+//Q4.
 const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
+    let books = await bookModel.find().populate([("author"),("publisher")]);
     res.send({data: books})
 }
 
-const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
-    res.send({data: specificBook})
 
-}
-
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.createBook= createBook;
+module.exports.getBooksData= getBooksData;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Nishant-R:cMVSc6ePV6V4dr03@cluster0.rembes2.mongodb.net/09-JUNE-2020", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
